5f6be1c584309e16344e496a8d73684ccb38b61b
Shivam Khandelwal <shivamchinu@gmail.com>
2014-01-30 23:18:30 +0530

    Moved most of the files to Library. No improvements to app functionality as of yet.

--- src/swilet/messenger/Messenger.java
+++ src/swilet/messenger/Messenger.java
@@ -5,7 +5,6 @@ import ibrdtn.api.object.EID;
 import ibrdtn.api.object.PayloadBlock;
 import ibrdtn.api.object.SingletonEndpoint;
 
-import ibrdtn.example.api.APIHandlerType;
 import ibrdtn.example.api.Constants;
 import ibrdtn.example.api.DTNClient;
 import java.io.BufferedReader;
@@ -17,10 +16,14 @@ import java.util.logging.Logger;
 
 public class Messenger {
 	private static final Logger logger = LogManager.getLogManager().getLogger("");
-    public static boolean isAutoResponse = false;
     private DTNClient dtnClient;
     protected String PRIMARY_EID = "messenger";
-    protected APIHandlerType HANDLER_TYPE = APIHandlerType.SELECTIVE;
+    
+    public Messenger() {
+        // Initialize connection to daemon
+        dtnClient = new DTNClient(PRIMARY_EID);
+        logger.log(Level.INFO, dtnClient.getConfiguration());
+    }
     
     public DTNClient getDtnClient() {
         return dtnClient;
@@ -30,15 +33,6 @@ public class Messenger {
         this.dtnClient = client;
     }
     
-    /**
-     * Creates a new DTN demonstration app.
-     */
-    public Messenger() {
-        // Init connection to daemon
-        dtnClient = new DTNClient(PRIMARY_EID, HANDLER_TYPE);
-        logger.log(Level.INFO, dtnClient.getConfiguration());
-    }
-
     private void exit() {
         dtnClient.shutdown();
         System.exit(0);
@@ -46,16 +40,10 @@ public class Messenger {
     
     private void send(String destinationAddress, String message) {
         EID destination = new SingletonEndpoint(destinationAddress);
-        
-//        SingletonEndpoint me = new SingletonEndpoint("api:me");
 
         // Create bundle to send
         Bundle bundle = new Bundle(destination, Constants.LIFETIME);
-        
         bundle.setPriority(Bundle.Priority.NORMAL);
-//        bundle.setReportto(me);
-//        bundle.setCustodian(me);
-//        bundle.setFlag(Bundle.Flags.CUSTODY_REQUEST, true);
         bundle.appendBlock(new PayloadBlock(message.getBytes()));
         dtnClient.send(bundle);    	
     }
@@ -64,11 +52,13 @@ public class Messenger {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	Messenger messenger = new Messenger();
     	String destination, message;
-    	System.out.print("Enter destination: ");
-    	destination = br.readLine();
-    	System.out.print("Enter message: ");
-    	message = br.readLine();
-    	messenger.send(destination, message);
-    	messenger.exit();
+    	while(true) {
+		    System.out.print("Enter destination: ");
+	    	destination = br.readLine();
+	    	System.out.print("Enter message: ");
+	    	message = br.readLine();
+	    	messenger.send(destination, message);
+	    }
+    	//messenger.exit();
     }
 }

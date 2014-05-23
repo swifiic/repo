dbd99bf6e05e6356d764fac58b7a29ba9f8dc12f
Shivam Khandelwal <shivamchinu@gmail.com>
2014-05-23 13:10:52 +0530

    Added new broadcast receiver

--- src/in/swifiic/android/app/suta/MainActivity.java
+++ src/in/swifiic/android/app/suta/MainActivity.java
@@ -1,39 +1,20 @@
 package in.swifiic.android.app.suta;
 
-import in.swifiic.android.app.lib.Helper;
 import in.swifiic.android.app.lib.ui.SwifiicActivity;
-import in.swifiic.android.app.lib.xml.Notification;
-import in.swifiic.android.app.suta.provider.Provider;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends SwifiicActivity {
 
-    private final String TAG="SUTA-MainAct";
+    @SuppressWarnings("unused")
+	private final String TAG="MainActivity";
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    	mDataReceiver= new BroadcastReceiver() {  // this is a must for all applications - hook to get notification from GenericService
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (intent.hasExtra("notification")) {
-                	String payload = intent.getStringExtra("notification");
-                    Log.d(TAG, "Handling incoming messages: " + payload);
-                    Notification notif = Helper.parseNotification(payload);
-                    handleNotification(notif);
-                } else {
-                    Log.d(TAG, "Broadcast Receiver ignoring message - no notification found");
-                }
-            }
-        };
-        
+        setContentView(R.layout.activity_main);        
     }
     
     @Override
@@ -47,36 +28,6 @@ public class MainActivity extends SwifiicActivity {
 		else {
 			return super.onOptionsItemSelected(item);
 		}
-    }
-
-    //private AppEndpointContext aeCtx = new AppEndpointContext("Messenger", "0.1", "1");
-
-    protected void handleNotification(Notification notif){
-    	// TODO
-    	// we need these arguments
-
-    	// lastSyncToHubReceivedAtHub
-    	// lastMessageReceivedAtHub
-    	// lastBillingDetails - free string ...
-    	// balance / credit
-    	// userAppListLastChangeTimestamp on appHub
-    	
-    	// userList <u name="" id="" alias="" />...
-    	// appList  <app name="" id="" alias=""> <role alias=""> <u id="" /> ... </role> <role...> </app>
-    	
-    	// Above was the grand goal - now simple implementation
-    	// userList "username|alias;username|alias;..."
-    	// appList  "appName|appId|appAlias|role1Alias:usrId:usrId:usrId|role2alias:...;
-    	
-    	if(null == Provider.providerInstance){
-    		Log.e(TAG,"No Provider - whatsup...?");
-    	} else {
-    		if(notif.getNotificationName().equals("DeviceListUpdate")) {
-    			String userList = notif.getArgument("userList");
-    			Log.d(TAG, "Got user list as: " + userList);
-    			Provider.providerInstance.loadUserSchema(userList);
-    		}		
-    	}
     }
 
 	@Override

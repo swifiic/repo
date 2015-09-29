49082b488085893eb060f070e0a37de360ec5ec8
aarthi1992 <aarthii92@gmail.com>
2015-09-29 11:16:00 +0530

    Messenger delete all messages issue

--- app/msngr/andi/src/in/swifiic/app/msngr/andi/MainActivity.java
+++ app/msngr/andi/src/in/swifiic/app/msngr/andi/MainActivity.java
@@ -10,9 +10,13 @@ import in.swifiic.plat.helper.andi.xml.Action;
 import java.util.Date;
 
 import android.app.ActionBar;
+import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
@@ -27,10 +31,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends SwifiicActivity {
     
@@ -39,24 +43,30 @@ public class MainActivity extends SwifiicActivity {
     private EditText messageToSend = null;
     private ListView conversation = null;
     private ImageButton b = null;
-    
     private AppEndpointContext aeCtx = new AppEndpointContext("Msngr", "0.1", "1");
+    
+   public static SharedPreferences pref=null;
     
     ConversationListCursorAdapter customAdapter;
     BroadcastReceiver mBroadcastReceiver;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);            	        // TODO check in sharedPreference if the generic code mentioned resetRequired
+        // TODO if yes - reset AND clear the value of resetRequired
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        pref=PreferenceManager.getDefaultSharedPreferences(this);
+        if(pref.getString("reset_required", "no").equalsIgnoreCase("yes")){
+        	DatabaseHelper db = new DatabaseHelper(this);
+        	db.deleteForReset();
+        	db.close();
+        	pref.edit().putString("reset_required", "no").commit();
+        }
         setContentView(R.layout.main_activity_msngr);
-        
         messageToSend = (EditText)findViewById(R.id.msgTextToSend);
-
-        messageToSend.requestFocus();
+	messageToSend.requestFocus();
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.showSoftInput(messageToSend, InputMethodManager.SHOW_IMPLICIT);
-
-
         conversation = (ListView)findViewById(R.id.conversation);
         b = (ImageButton)findViewById(R.id.buttonSendMsg);
         
@@ -102,7 +112,7 @@ public class MainActivity extends SwifiicActivity {
             		String sentAt = "" + date.getTime();
                     
             		Action act = new Action("SendMessage", aeCtx);
-                    act.addArgument("message", message);
+            		act.addArgument("message", message);
                     act.addArgument("toUser", toUser);
                     act.addArgument("fromUser", fromUser);
                     act.addArgument("sentAt", "" + sentAt);   
@@ -129,10 +139,37 @@ public class MainActivity extends SwifiicActivity {
             }
         });
     }
-
+    public static void restart(Context context, int delay) {
+        if (delay == 0) {
+            delay = 1;
+        }
+        Log.e("", "restarting app");
+        Intent restartIntent = context.getPackageManager()
+                .getLaunchIntentForPackage(context.getPackageName() );
+        PendingIntent intent = PendingIntent.getActivity(
+                context, 0,
+                restartIntent, Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        manager.set(AlarmManager.RTC, System.currentTimeMillis() + delay, intent);
+        System.exit(2);
+    }
+    
     public void onResume() {
         super.onResume();
-                
+      
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        pref=PreferenceManager.getDefaultSharedPreferences(this);
+        if(pref.getString("reset_required", "no").equalsIgnoreCase("yes")){
+        	DatabaseHelper db = new DatabaseHelper(this);
+        	db.deleteForReset();
+        	db.close();
+        	pref.edit().putString("reset_required", "no").commit();
+        	restart(this,0);
+        	
+        	
+        }
+      
+      
         final ActionBar actionBar = getActionBar();
     	new Handler().post(new Runnable() {
         	@Override
@@ -154,8 +191,8 @@ public class MainActivity extends SwifiicActivity {
                 conversation.setSelection(conversation.getCount());
                 conversation.smoothScrollToPosition(conversation.getCount());
             }
-        }, 100);
-    }
+        }, 100);}
+    
     
     /*
      * Menu Creation and handling

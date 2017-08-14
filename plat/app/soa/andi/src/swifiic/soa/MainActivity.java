224a271491eabb05f255e840f656652c4b840511
arnav dhamija <arnav.dhamija@gmail.com>
2017-08-14 23:03:48 +0530

    Build fails

--- plat/app/soa/andi/src/swifiic/soa/MainActivity.java
+++ plat/app/soa/andi/src/swifiic/soa/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -21,6 +22,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -29,9 +35,16 @@ import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.CookieHandler;
 import java.net.CookieManager;
+import java.net.URI;
 import java.util.ArrayList;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
@@ -43,11 +56,16 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 	private ActionBar actionBar;
 	// Tab titles
 	//private Resources res =
-	private String[] tabs =  null;
+	private String[] tabs = null;
 
 	// get the shared preferences
 	public static final SharedPreferences settings = AuthenticationActivity.getSettings();
 	public static final String IP_ADDRESS = "IP_ADDRESS";
+	/**
+	 * ATTENTION: This was auto-generated to implement the App Indexing API.
+	 * See https://g.co/AppIndexing/AndroidStudio for more information.
+	 */
+	private GoogleApiClient client;
 
 
 	@Override
@@ -66,7 +84,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 		actionBar.setHomeButtonEnabled(false);
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-		tabs =  new String[]{getResources().getString(R.string.Recharge),
+		tabs = new String[]{getResources().getString(R.string.Recharge),
 				getResources().getString(R.string.addUser),
 				getResources().getString(R.string.Users),
 				getResources().getString(R.string.swifiic)};
@@ -96,24 +114,28 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 			}
 		});
 
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 	}
 
 	// enables the swapping after disabling
-	public void enableSwapping(){
+	public void enableSwapping() {
 		viewPager.setBlockSwipe(false);
 	}
+
 	// disables the swap , when the user data is being edited
-	public void disableSwapping(){
+	public void disableSwapping() {
 		viewPager.setBlockSwipe(true);
 	}
 
 	/* Handles the backpress events ,
-        * asking for confirmation of the lost changes , deletion of an user , incompletely filled data
+		* asking for confirmation of the lost changes , deletion of an user , incompletely filled data
      */
 	@Override
 	public void onBackPressed() {
-		final AddUserFragment f = (AddUserFragment)getSupportFragmentManager().findFragmentByTag("EDIT_FRAGMENT");
-		if (f!=null && f.isVisible()) {
+		final AddUserFragment f = (AddUserFragment) getSupportFragmentManager().findFragmentByTag("EDIT_FRAGMENT");
+		if (f != null && f.isVisible()) {
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			Resources res = getResources();
 			builder.setMessage(res.getString(R.string.ChangesWillBeLost));
@@ -128,8 +150,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 			builder.setNegativeButton(res.getString(R.string.Cancel), null);
 			AlertDialog dialog = builder.create();
 			dialog.show();
-		}
-		else super.onBackPressed();
+		} else super.onBackPressed();
 	}
 
 	// inflates the menu
@@ -144,14 +165,15 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		//new LogoutTask().execute(ipAddr);
-		switch(item.getItemId()){
+		switch (item.getItemId()) {
 			case R.id.log_out:
 				logOut();
 				break;
 			case R.id.refresh_users:
-				ManageUserFragment.refresh(); break;
+				ManageUserFragment.refresh();
+				break;
 			case R.id.action_settings:
-				final Dialog dialog = new Dialog(MainActivity.this,android.R.style.Theme_Black);
+				final Dialog dialog = new Dialog(MainActivity.this, android.R.style.Theme_Black);
 				dialog.setContentView(R.layout.change_settings);
 				dialog.setTitle(getResources().getString(R.string.action_settings));
 				final EditText etIpAddr = (EditText) dialog.findViewById(R.id.etIpAddress);
@@ -162,11 +184,11 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 				bSave.setOnClickListener(new OnClickListener() {
 					@Override
 					public void onClick(View v) {
-						String ipAddr =etIpAddr.getText().toString().trim();
-						if (ipAddr==null || ipAddr.equals("")) toast("Please fill..!!");
+						String ipAddr = etIpAddr.getText().toString().trim();
+						if (ipAddr == null || ipAddr.equals("")) toast("Please fill..!!");
 						else {
 							SharedPreferences.Editor editor = settings.edit();
-							editor.putString(URL,ipAddr);
+							editor.putString(URL, ipAddr);
 							editor.commit();
 							toast("Changes saved!!");
 
@@ -180,7 +202,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 		return true;
 	}
 
-	public void logOut(){
+	public void logOut() {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		Resources res = getResources();
 		builder.setMessage(res.getString(R.string.logging_out));
@@ -195,18 +217,56 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 		dialog.show();
 	}
 
+	/**
+	 * ATTENTION: This was auto-generated to implement the App Indexing API.
+	 * See https://g.co/AppIndexing/AndroidStudio for more information.
+	 */
+	public Action getIndexApiAction() {
+		Thing object = new Thing.Builder()
+				.setName("Main Page") // TODO: Define a title for the content shown.
+				// TODO: Make sure this auto-generated URL is correct.
+				.setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+				.build();
+		return new Action.Builder(Action.TYPE_VIEW)
+				.setObject(object)
+				.setActionStatus(Action.STATUS_TYPE_COMPLETED)
+				.build();
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		client.connect();
+		AppIndex.AppIndexApi.start(client, getIndexApiAction());
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		AppIndex.AppIndexApi.end(client, getIndexApiAction());
+		client.disconnect();
+	}
+
 	public class ForceLogoutThread implements Runnable {
 		String msg = null;
-		ForceLogoutThread(String msg){
+
+		ForceLogoutThread(String msg) {
 			this.msg = msg;
 		}
+
 		@Override
 		public void run() {
 			forceLogout(msg);
 		}
 	}
 
-	public void forceLogout(String msg){
+	public void forceLogout(String msg) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		Resources res = getResources();
 		builder.setMessage(msg);
@@ -223,28 +283,44 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
 
 	//Logs out the user , removes his/her data from the shared preferences
-	public class LogOutTask extends AsyncTask<Void,Void,Void>{
+	public class LogOutTask extends AsyncTask<Void, Void, Void> {
 		@Override
 		protected Void doInBackground(Void[] params) {
 
 			try {
-				String URL = AuthenticationActivity.getUrl();
+				String URL = AuthenticationActivity.getUrl(); // this should be renamed!
 				ArrayList<BasicNameValuePair> nmPairs =
 						new ArrayList<BasicNameValuePair>(AuthenticationActivity.getCurNamevaluePairs());
 				//runOnUiThread(new ToastThread(nmPairs.con
-				nmPairs.add(new BasicNameValuePair("name","Logout"));
-				HttpClient httpclient = new DefaultHttpClient();//();
-				//HttpClient httpclient = createHttpClient();
-				HttpPost httppost = new HttpPost("http://"+URL+"/hubSrvr/Oprtr");
-				httppost.setEntity(new UrlEncodedFormEntity(nmPairs));
-				HttpResponse response = httpclient.execute(httppost);
-			}
-			catch(HttpHostConnectException e){
+				nmPairs.add(new BasicNameValuePair("name", "Logout"));
+
+				URL url = new URL("http://" + URL + "/hubSrvr/Oprtr");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setDoOutput(true);
+				conn.setChunkedStreamingMode(50); //??? What value to be used here?
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+				conn.setRequestProperty("charset", "utf-8");
+
+				Uri.Builder builder = new Uri.Builder();
+
+				for (BasicNameValuePair bnvp: nmPairs) {
+					builder.appendQueryParameter(bnvp.getName(), bnvp.getValue());
+				}
+				String query = builder.build().getEncodedQuery();
+				try (BufferedWriter bw = new BufferedWriter(conn.getOutputStream())) {
+					bw.write(query);
+				} catch (IOException e) {
+
+				}
+
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (HttpHostConnectException e) {
 				runOnUiThread(new ToastThread(getResources().getString(R.string.HostConnRefused)));
 
 
-			}
-			catch(Exception e){
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 
@@ -254,10 +330,10 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 				editor.remove(Constants.USERNAME);
 				editor.commit();
 				runOnUiThread(new ToastThread(getResources().getString(R.string.logging_out)));
-				startActivity(new Intent(MainActivity.this,AuthenticationActivity.class));
+				startActivity(new Intent(MainActivity.this, AuthenticationActivity.class));
 				finish();
 
-			}catch(Exception e){
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			return null;
@@ -267,11 +343,13 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
 	private class ToastThread implements Runnable {
 		String msg = null;
-		ToastThread(String msg){
-			this.msg  = msg;
+
+		ToastThread(String msg) {
+			this.msg = msg;
 		}
+
 		@Override
-		public void run(){
+		public void run() {
 			toast(msg);
 		}
 	}
@@ -292,8 +370,8 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 	}
 
 	// displays a toast message
-	public void toast(String s){
-		Toast.makeText(MainActivity.this,s, Toast.LENGTH_SHORT).show();
+	public void toast(String s) {
+		Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
 	}
 
 }
